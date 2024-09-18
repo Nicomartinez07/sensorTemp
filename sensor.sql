a784df2766db3584a7fc1d9c919e36231a657145
Nicolas Martinez <nicolasmartinezalfonso@gmail.com>
2024-09-18 16:56:04 -0300

    hice la adaptacion al programa

---
+++ sensor.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS TemperatureData (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        temperature REAL
+    )
--- sensor/__init__.py
+++ sensor/__init__.py
@@ -1,8 +1,26 @@
+from datetime import datetime
+import sqlite3
 from flask import Flask, request
+
+db_path = 'sensor.sqlite'
+conn = sqlite3.connect(db_path)
+# Ensure the table exists
+
+with open("sensor.sql") as f:
+    conn.executescript(f.read())
 
 app = Flask(__name__)
 
 @app.route('/temperatura', methods=('POST',))
 def hello():
-    temp = request.form['sensor']
+    temp = float(request.form['sensor'])
+    print(f'Sensor Temperatura: {temp}')
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    conn = sqlite3.connect(db_path)
+    conn.execute('''
+                        INSERT INTO TemperatureData (timestamp, temperature)
+                        VALUES (?, ?)
+                    ''', (timestamp, temp))
+    conn.commit()    
+    conn.close()
     return f'Sensor Temperatura: {temp}'
