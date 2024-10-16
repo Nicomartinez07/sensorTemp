6625668933dfa98ba39e897ce62d554840e98906
Nicolas Martinez <nicolasmartinezalfonso@gmail.com>
2024-10-16 16:43:40 -0300

    Codigo de sensor de sensores hecho

--- sensorEstacion/sensor/__init__.py
+++ sensorEstacion/sensor/__init__.py
@@ -6,25 +6,42 @@ db_path = 'sensores.sqlite'
 conn = sqlite3.connect(db_path)
 # Ensure the table exists
 
-with open("sensores.sql") as f:
+with open("sensor.sql") as f:
     conn.executescript(f.read())
 
 app = Flask(__name__)
 
 @app.route('/sensores', methods=('POST',))
 def hello():
-    temp = float(request.form['sensor'])
-    print(f'Sensor Temperatura: {temp}')
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    hum = float(request.form['sensor'])
-    press = float(request.form['sensor'])
-    light = float(request.form['sensor'])
-    water = float(request.form['sensor'])
+    temp = float(request.form['tempDHT'])
+    print(f'Sensor Temperatura: {temp}')
+    hum = float(request.form['humDHT'])
+    press = float(request.form['presionBMP'])
+    light = float(request.form['luzLDR'])
+    water = float(request.form['nivelAgua'])
     conn = sqlite3.connect(db_path)
     conn.execute('''
-                        INSERT INTO TemperatureData (timestamp, temperature, humidity, pressure, lightLevel, waterLevel)
+                        INSERT INTO Sensores (timestamp, temperature, humidity, pressure, lightLevel, waterLevel)
                         VALUES (?, ?, ?, ?, ?, ?)
-                    ''', (timestamp, temp, humidity, pressure, lightLevel, waterLevel)) 
+                    ''', (timestamp, temp, hum, press, light, water)) 
     conn.commit()    
     conn.close()
-    return f'Sensor Temperatura: {temp}'
+
+    print( f"""Sensor Temperatura: {temp}
+                Sensor Humedad: {hum}
+                Sensor Presion: {press}
+                Sensor Luz: {light}
+                Sensor Agua: {water}
+
+            """)
+
+    return f"""Sensor Temperatura: {temp}
+                Sensor Humedad: {hum}
+                Sensor Presion: {press}
+                Sensor Luz: {light}
+                Sensor Agua: {water}
+
+            """
+
+
